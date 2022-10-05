901477d17ef399759de020ab64bc57f44294be60
A01254093ASML <112582790+A01254093ASML@users.noreply.github.com>
2022-10-05 12:47:21 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,66 @@
-let primer_numero = 0
-let segundo_numero = 0
-let tercer_numero = 0
+let N1 = 0
+let N2 = 0
+let N3 = 0
+let mayor = 0
+let medio = 0
+let menor = 0
 input.onButtonPressed(Button.A, function () {
-    if (primer_numero == 0 && primer_numero == 0) {
+    if (N1 == 0 && N2 == 0) {
         basic.showIcon(IconNames.No)
-    } else {
-    	
+    }
+    if (N1 > N2 && N1 > N3) {
+        mayor = N1
+        if (N2 > N3) {
+            medio = N2
+            menor = N3
+        } else {
+            medio = N3
+            menor = N2
+        }
+        basic.showNumber(mayor)
+        basic.showNumber(medio)
+        basic.showNumber(menor)
+    }
+    if (N2 > N1 && N2 > N3) {
+        mayor = N2
+        if (N1 > N3) {
+            medio = N1
+            menor = N3
+        } else {
+            medio = N3
+            menor = N1
+        }
+        basic.showNumber(mayor)
+        basic.showNumber(medio)
+        basic.showNumber(menor)
+    }
+    if (N3 > N1 && N3 > N2) {
+        mayor = N3
+        if (N1 > N2) {
+            medio = N1
+            menor = N2
+        } else {
+            medio = N2
+            menor = N1
+        }
+        basic.showNumber(mayor)
+        basic.showNumber(medio)
+        basic.showNumber(menor)
     }
 })
 input.onGesture(Gesture.Shake, function () {
-    primer_numero = randint(0, 9)
-    segundo_numero = randint(0, 9)
-    tercer_numero = randint(0, 9)
-    if (primer_numero != segundo_numero && (segundo_numero != tercer_numero && primer_numero != tercer_numero)) {
+    N1 = randint(0, 9)
+    N2 = randint(0, 9)
+    N3 = randint(0, 9)
+    if (N1 != N2 && (N2 != N3 && N1 != N3)) {
         basic.showIcon(IconNames.Yes)
-        basic.showNumber(primer_numero)
-        basic.showNumber(segundo_numero)
-        basic.showNumber(tercer_numero)
+        basic.showNumber(N1)
+        basic.showNumber(N2)
+        basic.showNumber(N3)
     } else {
         basic.showIcon(IconNames.No)
-        primer_numero = 0
-        segundo_numero = 0
-        tercer_numero = 0
+        N1 = 0
+        N2 = 0
+        N3 = 0
     }
 })
